c657ef8c28db3ee69da4b94a41c1059ea782b079
LucasCorreia01 <lucascorreiadamaceno@gmail.com>
2023-01-02 13:59:03 -0300

    finished

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -75,15 +75,26 @@ class AppController extends Action {
 
         $usuarios = array();
 
+        $usuario = Container::getModel('usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
         if($pesquisarPor != "") {
 
-            $usuario = Container::getModel('usuario');
             $usuario->__set('nome', $pesquisarPor);
-            $usuario->__set('id', $_SESSION['id']);
+           
 
             $usuarios = $usuario->getAll();
 
+           
+
         }
+
+        $this->view->info_usuario = $usuario->getInfoUsuario();
+        $this->view->total_tweets = $usuario->getTotalTweets();
+        $this->view->total_seguindo = $usuario->getTotalSeguindo();
+        $this->view->total_seguidores = $usuario->getTotalSeguidores();
+        
+
 
         $this->view->usuarios = $usuarios;
 
